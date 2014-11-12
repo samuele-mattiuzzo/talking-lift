a3dccfe9d353920a8f559ae6b9212e0825625229
Samuele Mattiuzzo <samumatt@gmail.com>
2014-11-12 02:58:49 +0000

    Improving js

--- static/script.js
+++ static/script.js
@@ -1,16 +1,24 @@
-
 function scramble(){
-    return things[Math.floor(Math.random() * things.length)];
+    // Picks a random number
+    return Math.floor(Math.random() * things.length);
 }
 
 function talking_lift(){
-    setInterval(function(){
-        var thing,
-            div;
-        
-        thing = scramble();
-        div = document.getElementById('shit');
-        div.innerHTML = thing;
+    var thing,
+        div;
 
-    }, 15000);
+    setInterval(function(){
+        
+        // Picks a random sentence, writes it in the div
+        // Plays the corresponding sound (TODO: replace test with proper)
+        thing = scramble();
+        div = document.getElementById(span_id);
+        div.innerHTML = things[thing];
+
+        // TODO: enable + replace this once audio files are ready
+        //var audio = new Audio(audio_things[thing]);
+        var audio = new Audio(audio_url);
+        audio.play();
+
+    }, timer);
 }
