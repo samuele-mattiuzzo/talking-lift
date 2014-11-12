a4a1d963ae0c41f4ce4be128135fef3de62d2532
Samuele Mattiuzzo <samumatt@gmail.com>
2014-11-12 04:08:41 +0000

    Mute status switching function. Audio controls disabled

--- static/script.js
+++ static/script.js
@@ -1,24 +1,30 @@
+var div = document.getElementById('shit'),
+    audio = document.getElementById('audio'),
+    play_status = document.getElementById('play_status'),
+    timer = 5000;
+
 function scramble(){
     // Picks a random number
     return Math.floor(Math.random() * things.length);
 }
 
 function talking_lift(){
-    var thing,
-        div,
-        audio = document.getElementById('audio');
-
     setInterval(function(){
-        
         // Picks a random sentence, writes it in the div
-        // Plays the corresponding sound (TODO: replace test with proper)
+        // Plays the corresponding sound
         thing = scramble();
-        div = document.getElementById(span_id);
         div.innerHTML = things[thing];
-        audio[thing].play();
+        //audio[thing].play();
 
     }, timer);
 }
 
-function mute_sounds(){}
-function enable_sounds(){}
+function mute_sounds(){
+    var is_muted = (play_status.className === 'mute') ? true : false;
+    //audio.forEach(function(audio_element) { audio_element.muted = !is_muted; });
+    play_status.className = (is_muted === false) ? 'mute' : 'play';
+}
+play_status.addEventListener('click', function (event) {
+  mute_sounds();
+  event.preventDefault();
+});
