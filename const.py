c5015c851b217b1aedd53625f1be6668232b02f8
Samuele Mattiuzzo <samumatt@gmail.com>
2014-11-12 02:23:43 +0000

    Proper python app and const file

---
+++ const.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+FLOORS = (
+    'FLOOR 1',
+    'FLOOR 2',
+    'FLOOR 3',
+    'FLOOR 4',
+    'GROUND FLOOR'
+)
+
+MOVEMENT = (
+    'GOING UP',
+    'GOING DOWN'
+)
+
+DOORS = (
+    'PLEASE MIND THE DOORS',
+    'DOORS CLOSING',
+    'DOORS OPENING'
+)
--- lift.py
+++ lift.py
@@ -1,17 +1,21 @@
-import os
-from flask import Flask, render_template
+# -*- encoding: utf-8 -*-
+
+from const import *
+import json
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def lift():
 
     css_url = url_for('static', filename='style.css')
     js_url = url_for('static', filename='script.js')
-    says = []
+    things = list(FLOORS) + list(MOVEMENT) + list(DOORS)
 
     return render_template(
         'index.html',
         css_url=css_url,
         js_url=js_url,
-        says=says)
+        things=json.dumps(things))
